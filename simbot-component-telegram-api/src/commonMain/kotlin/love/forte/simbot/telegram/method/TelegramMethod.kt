a13eca7d626f45e1f4b4640af3d055e6a75384c5
ForteScarlet <ForteScarlet@163.com>
2024-02-26 01:34:04 +0800

    mark TODO

---
+++ simbot-component-telegram-api/src/commonMain/kotlin/love/forte/simbot/telegram/method/TelegramMethod.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024. ForteScarlet.
+ *
+ * This file is part of simbot-component-telegram.
+ *
+ * simbot-component-telegram is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * simbot-component-telegram is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with simbot-component-telegram.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package love.forte.simbot.telegram.method
+
+import kotlinx.serialization.DeserializationStrategy
+
+
+/**
+ *
+ * @author ForteScarlet
+ */
+public sealed class TelegramMethod<T, R : Any> {
+    // TODO 独立出 method?
+    //  sealed
+    //    -> Empty (Get & Post)
+    //    -> JSON (Post Only)
+    //    -> FORM (Post Only)
+
+    public abstract val name: String
+
+    public abstract val body: T
+
+    /**
+     * The result's [DeserializationStrategy] of this Method.
+     */
+    public abstract val resultDeserializationStrategy: DeserializationStrategy<R>
+}
