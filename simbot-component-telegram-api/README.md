bc4571b7078cecf758ddc793a4e50777e425591e
ForteScarlet <ForteScarlet@163.com>
2024-03-02 18:39:21 +0800

    README and module script

--- simbot-component-telegram-api/README.md
+++ simbot-component-telegram-api/README.md
@@ -9,3 +9,11 @@ val api = GetMeApi.create()
 
 val result = api.requestData(client, "botTOKEN")
 ```
+
+# Package love.forte.simbot.telegram.api
+
+The Telegram API and [Telegram methods](https://core.telegram.org/bots/api#available-methods).
+
+# Package love.forte.simbot.telegram.type
+
+The [Telegram types'](https://core.telegram.org/bots/api#available-types) class definitions.
--- simbot-component-telegram-api/build.gradle.kts
+++ simbot-component-telegram-api/build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2024. ForteScarlet.
+ *
+ * This file is part of simbot-component-telegram.
+ *
+ * simbot-component-telegram is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * simbot-component-telegram is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with simbot-component-telegram.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import love.forte.gradle.common.core.project.setup
 import love.forte.gradle.common.kotlin.multiplatform.applyTier1
 import love.forte.gradle.common.kotlin.multiplatform.applyTier2
@@ -14,7 +31,7 @@ setup(P.ComponentTelegram)
 
 useK2()
 configJavaCompileWithModule("simbot.component.telegram.api")
-apply(plugin = "simbot-telegram-multiplatform-maven-publish")
+// apply(plugin = "simbot-telegram-multiplatform-maven-publish")
 
 //configJsTestTasks()
 
